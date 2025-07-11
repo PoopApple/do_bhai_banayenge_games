f04f7c9fe9e7aa260766afad60bfa366d450ad1b
PoopApple <aryan1024sethi@gmail.com>
2025-07-11 18:26:00 +0530

    a

--- main_flask.py
+++ main_flask.py
@@ -1,5 +1,47 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import pymongo
+import datetime
+
+uri = "mongodb+srv://Atriays:VRkDtlmnmxyYnQZM@do-bhai-banayenge-games.bmuo0ev.mongodb.net/?retryWrites=true&w=majority&appName=do-bhai-banayenge-games"
+
+myclient = MongoClient(uri, server_api=ServerApi('1'))
+
+db_name = "games_all"
+table_name = "flappy_leaderboard"
+
+mydb = myclient[db_name]
+flappy_table = mydb[table_name]
+
+
+try:
+    myclient.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+    
+    
+dblist = myclient.list_database_names()
+if db_name in dblist:
+  print("The database exists.")
+else:
+    print("not exists")
+    
+
+time_of_score = datetime.datetime.now().strftime("%d %b %Y  %I:%M %p")
+lb_trial = {"name" : "abcd" , "score" : 1223, "date_time" : time_of_score}
+def ins():
+    insert_id = flappy_table.insert_one(lb_trial)
+    print(insert_id)
+
+query = {"name": "abc"}
+
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,7 +55,8 @@ def submit():
 
 @app.route('/leaderboard')
 def leaderboardView():
-    return render_template('leaderboard.html')  # show the form
+    lb_scores = flappy_table.find().sort("score",pymongo.DESCENDING)
+    return render_template('leaderboard.html' , scores = lb_scores)  # show the form
 
 if __name__ == '__main__':
     app.run(debug=True)
