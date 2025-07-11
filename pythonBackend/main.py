5ae0bfa5dfaa16eeeb668b816df1ae7fbae93e52
PoopApple <aryan1024sethi@gmail.com>
2025-07-11 16:50:02 +0530

    basic flask n mongo

--- pythonBackend/main.py
+++ pythonBackend/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+from pymongo import MongoClient
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,5 +11,13 @@ def submit():
     name = request.form['name']  # get form input
     return render_template('result.html', name=name)  # pass it to result page
 
+@app.route('/leaderboard')
+def leaderboardView():
+    return render_template('leaderboard.html')  # show the form
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
